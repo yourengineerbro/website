5fb9aeed8fdb7cf53315ff78a046f8f32ddf689a
Aman Gupta <guptaaman9981@gmail.com>
2024-11-27 13:57:55 +0530

    index.html

--- src/components/ImageSection.js
+++ src/components/ImageSection.js
@@ -4,7 +4,7 @@ const ImageSection = () => {
   return (
     <div className="image-section">
       <img
-        src="/image.png" /* Replace with your image */
+        src="/image2.png" /* Replace with your image */
         alt="Water Splash Shape"
       />
     </div>
